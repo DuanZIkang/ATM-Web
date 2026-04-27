36ca0a35485c098ce97b2e06edb715a70bb2f8c7
vaedu <vaedu070418@gmail.com>
2026-04-28 00:50:43 +0800

    1.perf: 优化冗余方法，提升性能

--- atm/src/main/java/com/example/atm/common/ApiResponse.java
+++ atm/src/main/java/com/example/atm/common/ApiResponse.java
@@ -1,32 +1,25 @@
 package com.example.atm.common;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ApiResponse<T> {
 
 	private boolean success;
 	private String message;
 	private T data;
 
-	public ApiResponse() {}
-
-	public ApiResponse(boolean success, String message, T data) {
-		this.success = success;
-		this.message = message;
-		this.data = data;
-	}
-
 	public static <T> ApiResponse<T> success(T data) {
 		return new ApiResponse<>(true, "ok", data);
 	}
 
-	public static <T> ApiResponse<T> fail(String message) {
-		return new ApiResponse<>(false, message, null);
-	}
 
-	// ===== Getter / Setter =====
+
 
 }
